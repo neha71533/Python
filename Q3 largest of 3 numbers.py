e2e7fd810dc5a58bcbdf059b9d418c4a82a6da19
neha71533 <nehasiji005@gmail.com>
2025-05-14 21:43:51 +0530

    Add files via upload

---
+++ Q3 largest of 3 numbers.py
@@ -0,0 +1,9 @@
+a= int(input("enter the number"))
+b= int(input("enter the number"))
+c= int(input("enter the number"))
+if(a>b)and(a>c):
+   print(f"{a} is greater")
+if (b>c)and(b>a):
+   print(f"{b} is greater")
+else :
+   print(f"{c} is greater")
