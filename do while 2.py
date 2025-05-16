7ff50fd7d0731b3502bfbc2c454a93a71eb6ab9c
neha71533 <nehasiji005@gmail.com>
2025-05-16 12:32:36 +0530

    Add files via upload

---
+++ do while 2.py
@@ -0,0 +1,9 @@
+while True:
+    name=input("enter the name:")
+    print(f"my name is{name}")
+    age=int(input("enter the age:"))
+    print(f" my age is{age}")
+    choice=input("enter your choice y/n")
+    if choice=="n":
+        break
+      
