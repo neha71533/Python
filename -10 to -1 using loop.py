c8ada7b582c79097caef1f2b93b0b48f2a7a8962
neha71533 <nehasiji005@gmail.com>
2025-05-16 13:25:44 +0530

    Add files via upload

---
+++ -10 to -1 using loop.py
@@ -0,0 +1,3 @@
+print("numbers are:")
+for i in range(-10,-1):
+    print(i)
---
+++ trial sum of series .py
@@ -0,0 +1,6 @@
+n=int(input("enter the limit"))
+print('enter no.')
+sum=0
+for i in(range(1,n+1)):
+        sum=sum+i
+print(sum)
